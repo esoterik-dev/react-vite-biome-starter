cfd8838d2613a7f1d90be0d528a8807159c002e9
Esoterik <33873170+esoterik-dev@users.noreply.github.com>
2025-07-18 12:13:08 +0530

    refactor: enhance linting scripts and add ESLint configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import './App.css';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
+import { useState } from "react";
+import "./App.css";
+import viteLogo from "/vite.svg";
+import reactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -25,7 +25,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
 }
